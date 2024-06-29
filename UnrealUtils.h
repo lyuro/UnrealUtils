1481909c4a72bbd87886dc92569a2a129aeed455
lyuro <38397800+lyuro@users.noreply.github.com>
2024-06-30 00:45:28 +0900

    Create UnrealUtils.h

---
+++ UnrealUtils.h
@@ -0,0 +1,18 @@
+// UnrealUtils.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Misc/AES.h"
+#include "Misc/Base64.h"
+
+namespace UnrealUtils
+{
+    namespace Common
+    {
+        FString Encrypt(const FString& InputString, const FAES::FAESKey& Key);
+        FString Decrypt(const FString& InputString, const FAES::FAESKey& Key);
+        FString EncryptBase64(const FString& InputString, const FAES::FAESKey& Key);
+        FString DecryptBase64(const FString& InputString, const FAES::FAESKey& Key);
+    }
+}
